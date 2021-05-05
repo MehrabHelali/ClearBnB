289bf79f03c02eb1cbd067046b0608b60216fdee
perfarhat <79327119+MehrabHelali@users.noreply.github.com>
2021-05-05 11:19:05 +0200

    sadasda

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <header></header>
+
+  <main>
+    <router-view />
+  </main>
+
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  async created() {
+    let res = await fetch('/rest/hello')
+    let message = await res.json()
+    console.log(message);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
